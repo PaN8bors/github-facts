d671ce4a360935242c7f35bed3b27501f07ae06e
Parish Neighbors <pn8bors@gmail.com>
2022-02-10 10:37:02 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Get info from github
---
+++ main.js
@@ -0,0 +1,36 @@
+import got from 'got';
+import input from 'input';
+
+
+
+async function github(username) {
+    const resp = got(`https://api.github.com/users/${username}/repos`)
+    const data = await resp.json();
+    return data;
+}
+
+function display(projects) {
+    const sorted = projects.sort((projectA, projectB) => {
+        const a = projectA.stargazers_count;
+        const b = projectB.stargazers_count;
+        if (a>b) {
+            return -1;
+        } else if (a<b) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    for (let project of sorted) {
+        console.log(`(${project.stargazers_count}) ${project.name}: ${project.description}`);
+    }
+}
+
+async function main() {
+    const resp = await input.text('What is your github username?');
+    const repos = await github(resp);
+    github(resp);
+    display(repos);
+}
+
+main();
